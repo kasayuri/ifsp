e164f6e782818a0411a8c55eafb62ec26cbc66bc
Karina <pste09.informatica@crecisp.gov.br>
2025-08-31 11:06:31 -0300

    Add BASE_URL and TOKEN constants and InvertextoApi interface
    This commit introduces two new constants, `BASE_URL` and `TOKEN`, in the `Constantes.java` file. It also creates a new interface `InvertextoApi.java` with a `getLogradouro` method for retrieving address information based on a given CEP number and token.

--- Dispositivos_Moveis_I/Modulo_3/Servicos/app/src/main/java/com/example/servicos/Constantes.java
+++ Dispositivos_Moveis_I/Modulo_3/Servicos/app/src/main/java/com/example/servicos/Constantes.java
@@ -1,4 +1,6 @@
 package com.example.servicos;
 
 public class Constantes {
+    public static String BASE_URL = "https://api.invertexto.com/";
+    public static String TOKEN = "";
 }
--- Dispositivos_Moveis_I/Modulo_3/Servicos/app/src/main/java/com/example/servicos/api/InvertextoApi.java
+++ Dispositivos_Moveis_I/Modulo_3/Servicos/app/src/main/java/com/example/servicos/api/InvertextoApi.java
@@ -1,4 +1,16 @@
 package com.example.servicos.api;
 
-interface InvertextoApi {
+import com.example.servicos.model.Logradouro;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface InvertextoApi {
+    @GET("/v1/cep/{numero}")
+    Call<Logradouro> getLogradouro(
+            @Path("numero") String numero,
+            @Query("token") String token
+    );
 }
